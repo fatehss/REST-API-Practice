6c0c953deff5bff060373b397cc70c2a1a66a78b
fateh <fatehss@g.ucla.edu>
2025-03-09 13:51:42 -0230

    add openapi generating script

---
+++ scripts/generate_openapi.py
@@ -0,0 +1,19 @@
+# scripts/generate_openapi.py
+
+import json
+from pathlib import Path
+import os
+import sys
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))) # add ../ to sys.path
+
+from app.server import app
+def generate_openapi_json():
+    openapi_json = app.openapi()
+    output_path = Path("openapi.json")
+    output_path.write_text(json.dumps(openapi_json, indent=2))
+    print(f"Generated {output_path}")
+
+if __name__ == "__main__":
+    generate_openapi_json()
+
